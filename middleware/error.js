f7a979d3a4c016378d907bb108b14b70960ed401
Suraj Shelke <surajshelke332@gmail.com>
2024-01-20 22:06:28 +0530

    Deploy

--- middleware/error.js
+++ middleware/error.js
@@ -9,7 +9,7 @@ const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
-    message: err.message,
+    message: error.message,
   });
 };
 
