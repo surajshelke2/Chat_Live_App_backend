578c236fe4c46bca794edcd4b0a1f73d7b686517
Suraj Shelke <surajshelke332@gmail.com>
2024-01-20 22:24:56 +0530

    remove the resolver

--- server.js
+++ server.js
@@ -31,17 +31,10 @@ app.use("/api/message", messageRouter);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-  );
-} else {
   app.get("/", (req, res) => {
     res.send("API is running..");
-  });
-}
+  })
+ 
 
 
 
